5eba3309a0880bd1ab8d49ee376f0900f1df9d8f
Павел <107019787+Pol888@users.noreply.github.com>
2023-06-03 09:01:56 +0500

    done2

--- Tree.java
+++ Tree.java
@@ -110,40 +110,7 @@ public class Tree{
 
 
 
-    //public boolean contains(V value){
-    //    Node node = root;
-    //    while (node != null){
-    //        if (node.value.equals(value)){
-    //            return true;
-    //        }
-    //        if (node.value.compareTo(value) > 0) {
-    //            node = node.left;
-    //        }else {
-    //            node = node.right;
-    //        }
-    //    }
-    //    return false;
-    //}
 
-    //решение рекурсией
- //   public boolean contains(V value) {
- //       if (root == null){
- //           return false;
- //       }
- //       return contains(root, value);
- //   }
-//
-    //private boolean contains(Node node, V value) {
-    //    if (node.value.equals(value)){
-    //        return true;
-    //    } else {
-    //        if (node.value.compareTo(value) > 0){
-    //            return contains(node.left, value);
-    //        } else {
-    //            return contains(node.right, value);
-    //        }
-    //    }
-    //}
 
     private enum Color
     {
